ca11c9878fceac3bf06feabe9e571514578fbbb3
MuneneCalvin <munenecalvn@gmail.com>
2022-07-01 04:41:41 -0700

    Print alphabt

---
+++ 0x01-variables_if_else_while/4-prints_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  * main - Entry point
+ *   * Description: prints lowercase alphabets
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
