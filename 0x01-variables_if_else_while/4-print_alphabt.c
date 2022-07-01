73fcda22744562805999c9acfd192cd4c6bf4605
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-01 15:43:33 +0300

    Add files via upload

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Description: prints lowercase alphabets
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; ch <= 'Z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
 		{
