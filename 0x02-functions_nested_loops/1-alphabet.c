aac5588f422d8209474ff0488dadcdcd7677b239
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-05 13:37:03 +0300

    Add files via upload

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - print alphabet lowercase
- *   *
- *    * Return: Always 0.
- *     */
+ * print_alphabet - print alphabet lowercase
+ *
+ * Return: Always 0.
+ */
 
 void print_alphabet(void)
 {
-	char alphabet;
+char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		_putchar(alphabet);
-	}
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
 
-	_putchar('\n');
+_putchar('\n');
 
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +10,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
