dd7b8bd37417950e1e6856720c118fe23218073f
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-11 12:33:22 +0300

    Add files via upload

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- *  * print_rev - prints a string, in reverse, followed by a new line.
- *   * @s: input string.
- *    * Return: no return.
- *     */
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
 void print_rev(char *s)
 {
-		int count = 0;
+	int count = 0;
 
-			while (count >= 0)
-					{
-								if (s[count] == '\0')
-												break;
-										count++;
-											}
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
-				for (count--; count >= 0; count--)
-							_putchar(s[count]);
-					_putchar('\n');
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- *  * print_array - prints n elements of an array of integers.
- *   * @a: input array.
- *    * @n: input n elements
- *     * Return: no return.
- *      */
+ * print_array - prints n elements of an array of integers.
+ * @a: input array.
+ * @n: input n elements
+ * Return: no return.
+ */
 void print_array(int *a, int n)
 {
-		int i = 0;
+	int i = 0;
 
-			for (; i < n; i++)
-					{
-								printf("%d", *(a + i));
-										if (i != (n - 1))
-														printf(", ");
-											}
-				printf("\n");
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
 }
