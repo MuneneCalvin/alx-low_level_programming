5bd97967511463fd9bd5596b88566c5eaebee7b2
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-11 12:04:35 +0300

    Add files via upload

--- 0x05-pointers_array_strings/2-strlen.c
+++ 0x05-pointers_array_strings/2-strlen.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  * _strlen - returns the length of a string.
- *   * @s: input stringeturn.
- *    * Return: length of a string.
- *     */
+ * _strlen - returns the length of a string.
+ * @s: input stringeturn.
+ * Return: length of a string.
+ */
 int _strlen(char *s)
 {
-		int count = 0;
+	int count = 0;
 
-			while (*(s + count) != '\0')
-						count++;
-				return (count);
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
 }
