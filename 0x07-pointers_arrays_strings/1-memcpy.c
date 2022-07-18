1790874c6ba8482f0b4df057d157bb6c4c5eeb03
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-18 12:07:19 +0300

    Add files via upload

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- *  * _memcpy - copies memory area,
- *   * @dest: destination memory area.
- *    * @src: source memory area.
- *     * @n: bytes filled.
- *      * Return: the pointer to dest.
- *       */
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-						*(dest + i) =  *(src + i);
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
 
-				return (dest);
+	return (dest);
 }
