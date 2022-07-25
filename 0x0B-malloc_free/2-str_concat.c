457cbcb0d6d44e44087020a7b6779f5f0896b829
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-07-25 11:58:04 +0300

    Add files via upload

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 
 /**
- *  * str_concat - concatenates two strings.
- *   * @s1: first string.
- *    * @s2: second string.
- *     *
- *      * Return: pointer of an array of chars
- *       */
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer of an array of chars
+ */
 char *str_concat(char *s1, char *s2)
 {
-		char *strout;
-			unsigned int i, j, k, limit;
+	char *strout;
+	unsigned int i, j, k, limit;
 
-				if (s1 == NULL)
-							s1 = "";
-					if (s2 == NULL)
-								s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-						for (i = 0; s1[i] != '\0'; i++)
-									;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
 
-							for (j = 0; s2[j] != '\0'; j++)
-										;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-								strout = malloc(sizeof(char) * (i + j + 1));
+	strout = malloc(sizeof(char) * (i + j + 1));
 
-									if (strout == NULL)
-											{
-														free(strout);
-																return (NULL);
-																	}
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
 
-										for (k = 0; k < i; k++)
-													strout[k] = s1[k];
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-											limit = j;
-												for (j = 0; j <= limit; k++, j++)
-															strout[k] = s2[j];
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
+	return (strout);
+}
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- *  * free_grid - frees a 2 dimensional grid.
- *   * @grid: multidimensional array of integers.
- *    * @height: height of the grid.
- *     *
- *      * Return: no return
- *       */
+ * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
+ * @height: height of the grid.
+ *
+ * Return: no return
+ */
 void free_grid(int **grid, int height)
 {
-		if (grid != NULL && height != 0)
-				{
-							for (; height >= 0; height--)
-											free(grid[height]);
-									free(grid);
-										}
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
