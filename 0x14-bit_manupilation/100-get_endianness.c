922c80d7b962e695758d7c6acde3e7ffee2cdbd7
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-08-18 13:58:10 +0300

    bit manupilation

--- 0x14-bit_manupilation/100-get_endianness.c
+++ 0x14-bit_manupilation/100-get_endianness.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
+
+
 /**
- *  * get_endianness - checks the endianness
- *   *
- *    * Return: 0 if big endian, 1 if little endian
- *     */
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
+
 {
-		unsigned int x;
-			char *c;
 
-				x = 1;
-					c = (char *) &x;
+	unsigned int x;
 
-						return ((int)*c);
+	char *c;
+
+
+
+	x = 1;
+
+	c = (char *) &x;
+
+
+
+	return ((int)*c);
+
 }
