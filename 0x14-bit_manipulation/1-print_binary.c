a056d444631459f53fc3e506adcf03ad4a5a67fd
MuneneCalvin <munenecalvn@gmail.com>
2022-08-19 01:09:08 -0700

    Password

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_binary - prints the binary representation
  * of a number.
@@ -10,14 +8,14 @@
  */
 void print_binary(unsigned long int n)
 {
-if (n >> 0)
-{
-if (n >> 1)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
-}
-else
-{
-_putchar('0');
-}
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
