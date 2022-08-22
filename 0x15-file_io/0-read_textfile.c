868c64c344728671de1888f7fdb63c42b2ab51b4
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2022-08-22 12:18:31 +0300

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- *  * read_textfile - reads a text file and prints the letters
- *   * @filename: filename.
- *    * @letters: numbers of letters printed.
- *     *
- *      * Return: numbers of letters printed. It fails, returns 0.
- *       */
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-		int fd;
-			ssize_t nrd, nwr;
-				char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-					if (!filename)
-								return (0);
+	if (!filename)
+		return (0);
 
-						fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-							if (fd == -1)
-										return (0);
+	if (fd == -1)
+		return (0);
 
-								buf = malloc(sizeof(char) * (letters));
-									if (!buf)
-												return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-										nrd = read(fd, buf, letters);
-											nwr = write(STDOUT_FILENO, buf, nrd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-												close(fd);
+	close(fd);
 
-													free(buf);
+	free(buf);
 
-														return (nwr);
+	return (nwr);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,39 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * append_text_to_file - appends text at the end of a file
- *   * @filename: filename.
- *    * @text_content: added content.
- *     *
- *      * Return: 1 if the file exists. -1 if the fails does not exist
- *       * or if it fails.
- *        */
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-		int fd;
-			int nletters;
-				int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-					if (!filename)
-								return (-1);
+	if (!filename)
+		return (-1);
 
-						fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-							if (fd == -1)
-										return (-1);
+	if (fd == -1)
+		return (-1);
 
-								if (text_content)
-										{
-													for (nletters = 0; text_content[nletters]; nletters++)
-																	;
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-															rwr = write(fd, text_content, nletters);
+		rwr = write(fd, text_content, nletters);
 
-																	if (rwr == -1)
-																					return (-1);
-																		}
+		if (rwr == -1)
+			return (-1);
+	}
 
-									close(fd);
+	close(fd);
 
-										return (1);
+	return (1);
 }
